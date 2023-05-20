c64a8d6fa76ac707da0db85ed750b9de9babcb11
Ibuki-Y <ibu.love.drum12@gmail.com>
2023-05-20 23:54:47 +0900

    feat: add CORS CSRF

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -14,6 +14,7 @@ type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
+	CSRFToken(c echo.Context) error
 }
 
 type userController struct {
@@ -76,4 +77,12 @@ func (u *userController) LogOut(c echo.Context) error {
 	c.SetCookie(cookie)
 
 	return c.NoContent(http.StatusOK)
+}
+
+func (u *userController) CSRFToken(c echo.Context) error {
+	token := c.Get("csrf").(string)
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
 }
--- router/router.go
+++ router/router.go
@@ -1,19 +1,36 @@
 package router
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/Ibuki-Y/go-echo-clean/controller"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		CookieSameSite: http.SameSiteDefaultMode,
+		// CookieSameSite: http.SameSiteNoneMode,
+		// CookieMaxAge: 60,
+	}))
 
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.GET("/csrf", uc.CSRFToken)
 
 	t := e.Group("/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
